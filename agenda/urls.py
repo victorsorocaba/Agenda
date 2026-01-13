e39c4f8cc894cdebcd855e6a76add315b4751a9a
Victor Emanuel Ferreira Moraes <t_victor.moraes@prefeitura.local>
2026-01-13 18:02:46 -0300

    Incluido função e tela de autenticação

--- agenda/urls.py
+++ agenda/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('eventos/<titulo_evento>/', views.eventos),
     path('agenda/', views.lista_eventos),
     path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user)
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 from core.models import Evento
 
@@ -7,10 +10,30 @@ from core.models import Evento
 # Create your views here.
 #def index(request):
     #return redirect('/agenda/')
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usúario ou senha inválido")
+
+    return redirect('/')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+def login_user(request):
+    return render(request, 'core/login.html')
+
 def eventos(request, titulo_evento):
     evento = Evento.objects.get(titulo= titulo_evento)
     return HttpResponse(evento.local)
 
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
     evento = Evento.objects.filter(usuario=usuario)
