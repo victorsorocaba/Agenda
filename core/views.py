094864eac4ac530235e69650e55db8bb48a55e89
Victor Emanuel Ferreira Moraes <t_victor.moraes@prefeitura.local>
2026-01-14 14:54:28 -0300

    adicionado o campo local

--- core/views.py
+++ core/views.py
@@ -50,8 +50,10 @@ def submit_evento(request):
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
+        local = request.POST.get('local')
         Evento.objects.create(titulo=titulo,
                               data_evento=data_evento,
                               descricao=descricao,
-                              usuario=usuario)
+                              usuario=usuario,
+                              local=local)
     return redirect('/')
